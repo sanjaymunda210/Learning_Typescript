f22bea69880bfbb5fbad8e66c57d81b52b2b142f
Sanjay <sanjaymunda0123@gmail.com>
2025-03-06 11:51:28 +0530

    Private Public

--- 03.PureTs/dist/index.js
+++ 03.PureTs/dist/index.js
@@ -1,11 +1,21 @@
 "use strict";
+// class User {
+//   public email: string;
+//   private name: string;
+//   private readonly city: string = "Jamshedpur";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+// const sanjay = new User("sanjay@gmail.com", "Sanjay");
+// sanjay.city = "Jamshedpur";
+// console.log(sanjay);
 class User {
-    constructor(email, name) {
-        this.city = "Jamshedpur";
+    constructor(email, name, userId) {
         this.email = email;
         this.name = name;
+        this.userId = userId;
+        this.city = "Jamshedpur";
     }
 }
-const sanjay = new User("sanjay@gmail.com", "Sanjay");
-// sanjay.city = "Jamshedpur";
-console.log(sanjay);
--- 03.PureTs/src/index.ts
+++ 03.PureTs/src/index.ts
@@ -1,13 +1,21 @@
-class User {
-  email: string;
-  name: string;
-  readonly city: string = "Jamshedpur";
-  constructor(email: string, name: string) {
-    this.email = email;
-    this.name = name;
-  }
-}
-
-const sanjay = new User("sanjay@gmail.com", "Sanjay");
+// class User {
+//   public email: string;
+//   private name: string;
+//   private readonly city: string = "Jamshedpur";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+// const sanjay = new User("sanjay@gmail.com", "Sanjay");
 // sanjay.city = "Jamshedpur";
-console.log(sanjay);
+// console.log(sanjay);
+
+class User {
+  private readonly city: string = "Jamshedpur";
+  constructor(
+    public email: string,
+    public name: string,
+    private userId: string
+  ) {}
+}
