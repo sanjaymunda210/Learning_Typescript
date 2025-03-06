5ab74ec2081578a4d25911afb03c3cd647d24812
Sanjay <sanjaymunda0123@gmail.com>
2025-03-06 12:06:46 +0530

    Getters and Setters

--- 03.PureTs/dist/index.js
+++ 03.PureTs/dist/index.js
@@ -12,10 +12,28 @@
 // sanjay.city = "Jamshedpur";
 // console.log(sanjay);
 class User {
-    constructor(email, name, userId) {
+    constructor(email, name // private userId: string
+    ) {
         this.email = email;
         this.name = name;
-        this.userId = userId;
+        this._courseCount = 1;
         this.city = "Jamshedpur";
     }
+    deleteToken() {
+        console.log(" Token Deleted ");
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be greater than 1");
+        }
+        this._courseCount = courseNum;
+    }
 }
+const sanjay = new User("sanjay@gmail.com", "Sanjay");
+// sanjay.deleteToken();
--- 03.PureTs/src/index.ts
+++ 03.PureTs/src/index.ts
@@ -12,10 +12,31 @@
 // console.log(sanjay);
 
 class User {
-  private readonly city: string = "Jamshedpur";
+  private _courseCount = 1;
+  readonly city: string = "Jamshedpur";
   constructor(
     public email: string,
-    public name: string,
-    private userId: string
+    public name: string // private userId: string
   ) {}
+
+  private deleteToken() {
+    console.log(" Token Deleted ");
+  }
+
+  get getAppleEmail(): string {
+    return `apple${this.email}`;
+  }
+
+  get courseCount(): number {
+    return this._courseCount;
+  }
+
+  set courseCount(courseNum) {
+    if (courseNum <= 1) {
+      throw new Error("Course count should be greater than 1");
+    }
+    this._courseCount = courseNum;
+  }
 }
+const sanjay = new User("sanjay@gmail.com", "Sanjay");
+// sanjay.deleteToken();
