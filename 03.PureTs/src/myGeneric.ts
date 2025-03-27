f95b6e752dedc5acc4a63f77b85696fded2cf89e
Sanjay <sanjaymunda0123@gmail.com>
2025-03-27 18:43:18 +0530

    Generic Classes

--- 03.PureTs/src/myGeneric.ts
+++ 03.PureTs/src/myGeneric.ts
@@ -40,3 +40,36 @@ const getMoreSearchProducts = <T>(products: T[]): T => {
   return products[myIndex];
 };
 // comma signifies the generic
+
+interface Database {
+  connection: string;
+  username: string;
+  password: string;
+}
+function anotherFunction<T, U extends Database>(valOne: T, valTwo: U): object {
+  return {
+    valOne,
+    valTwo,
+  };
+}
+
+// anotherFunction(3, { username: "sanjay", password: "1234", connection: "localhost" });
+
+interface Quiz {
+  name: string;
+  type: string;
+}
+
+interface Course {
+  name: string;
+  author: string;
+  subject: string;
+}
+
+class Sellable<T> {
+  public cart: T[] = [];
+
+  addToCart(product: T) {
+    this.cart.push(product);
+  }
+}
